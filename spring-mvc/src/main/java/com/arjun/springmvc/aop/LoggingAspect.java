2e162418eeeeb5b0e37cbeb988f4af582231dda4
arjungautam1 <arjungautam5431@gmail.com>
2021-05-28 16:13:45 +0545

    Spring for log

--- spring-mvc/src/main/java/com/arjun/springmvc/aop/LoggingAspect.java
+++ spring-mvc/src/main/java/com/arjun/springmvc/aop/LoggingAspect.java
@@ -5,17 +5,32 @@
  */
 package com.arjun.springmvc.aop;
 
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 
 @Aspect
 @Component
 public class LoggingAspect {
 
+    private static final Logger LOGGER = LoggerFactory.getLogger(LoggingAspect.class);
+
     @Before("execution(public * com.arjun.springmvc.controller.StudentController.getStudents())")
-    public void log() {
-        System.out.println("Getting all data from Student");
+    public void logBefore() {
+        LOGGER.info("Student got.");
     }
+
+    //this works as finally in exception
+    @After("execution(public * com.arjun.springmvc.controller.StudentController.getStudents())")
+    public void logAfter() {
+        LOGGER.info("Student got using after");
+    }
+
+//    instead we can use @AfterReturning only if code works
+
+    //@AfterThrowing for exception
 
 }
