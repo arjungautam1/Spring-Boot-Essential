7b0ef9861ac29031e7925fd9bfe89c9e21f7ba76
arjungautam1 <arjungautam5431@gmail.com>
2021-05-29 19:50:18 +0545

    containrizing spring app with docker

---
+++ booting-web/src/test/java/com/arjun/bootingweb/BootingWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arjun.bootingweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootingWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
